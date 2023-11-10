242664a4675e6ab4c5289f60115dfdf2ff8096d7
Jayakumar N <jayagma032@gmail.com>
2023-11-10 16:54:29 +0530

    test remove db uri

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -41,6 +41,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://tentukotta:148644Jan@tentukottadb.7tgi6ko.mongodb.net/?retryWrites=true&w=majority"))
+    DATABASE_URL = str(getenv('DATABASE_URL', ""))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
