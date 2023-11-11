91fbc6f5ebc92e6664f3b5f278597305288ab025
Jayakumar N <jayagma032@gmail.com>
2023-11-11 07:53:30 +0530

    Update FQDN

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -23,14 +23,14 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001587191598'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'streaming.jk1812.workers.dev'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1001721054").split())
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
+    APP_NAME = "Cloudflare"
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
     if 'DYNO' in environ:
-        ON_HEROKU = True
+        ON_HEROKU = False
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
