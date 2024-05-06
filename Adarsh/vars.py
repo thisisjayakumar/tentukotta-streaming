b76b8e58d416478d4b1b8dc345f17c6d9f9f84e8
Jayakumar N <jayagma032@gmail.com>
2024-05-06 10:33:58 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -42,6 +42,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://tentukottamovie:148644@tentukottamovie.s1pkcuk.mongodb.net/?retryWrites=true&w=majority&appName=tentukottamovie"))
+    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://tentukottamovies:148644@tentukottamovies.od3iiyd.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))  
